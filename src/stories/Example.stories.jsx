09ff02128938b196bd31db6d8a3e0cb70d42a2d2
pfi4369 <rick.wells@healthtrustpg.com>
2026-01-08 16:13:27 -0600

    Add Example story for Hello component

---
+++ src/stories/Example.stories.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default {
+  title: 'Example/Hello',
+};
+
+export const Hello = () => <div style={{ padding: 20, fontFamily: 'sans-serif' }}>Hello Storybook</div>;
