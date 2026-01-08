fc0291873249bda6be69d23555f6e4bb87b58760
pfi4369 <rick.wells@healthtrustpg.com>
2026-01-08 16:18:08 -0600

    Refactor Hello story to use React.createElement

--- src/stories/Example.stories.jsx
+++ src/stories/Example.stories.jsx
@@ -4,4 +4,9 @@ export default {
   title: 'Example/Hello',
 };
 
-export const Hello = () => <div style={{ padding: 20, fontFamily: 'sans-serif' }}>Hello Storybook</div>;
+export const Hello = () =>
+  React.createElement(
+    'div',
+    { style: { padding: 20, fontFamily: 'sans-serif' } },
+    'Hello Storybook'
+  );
