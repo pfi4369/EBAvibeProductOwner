759fbd927d831292d7eda9414e9ba62c670464c1
pfi4369 <rick.wells@healthtrustpg.com>
2026-01-08 15:16:34 -0600

    Initialize Storybook configuration in main.js

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+module.exports = {
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  framework: {
+    name: "@storybook/react",
+    options: {}
+  },
+  core: {
+    builder: "webpack5"
+  }
+};
